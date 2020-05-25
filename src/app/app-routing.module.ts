60a9ecae2246e74d7a9885f53ee1b5df6a9dd894
GustavoSantOliveira <gustavosantana86@hotmail.com>
2020-05-25 17:44:00 -0300

    inclusão service e componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AvaliacaoComponent } from './avaliacao/avaliacao.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'avaliacao',
+    component: AvaliacaoComponent,
+    data: { title: 'Avaliação' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/service/analytics.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = '';
+
+export class AnalyticsService {
+
+  constructor(private http: HttpClient) { }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      return of(result as T);
+    };
+  }
+}
